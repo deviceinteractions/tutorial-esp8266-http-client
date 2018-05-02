3fc0f6fe4a3b4660a7974bd3a6a0371292283ee1
Ranjit Bhinge <ranjitbhinge@phaneronsystems.com>
2018-05-02 10:50:25 +0530

    updated comments

--- WiFiClientBasic-google-get.ino
+++ WiFiClientBasic-google-get.ino
@@ -1,6 +1,14 @@
 /*  Surfing the internet with the ESP8266
-    Written using the WiFIClientBasic Example Code
-    20-Apr-18, Device Interactions
+    Written using the WiFiClientBasic Example Code
+    
+    Ranjit Bhinge,
+    Device Interactions,
+    20-Apr-2018
+    
+    www.device-interactions.com
+    shop.device-interactions.com
+    blog.device-interactions.com
+    www.github.com/deviceinteractions/
 
     This code lets the ESP8266 connect to any website using WiFi
     It shows you the html response received by the ESP8266 on the Serial Monitor
@@ -18,6 +26,10 @@
     *Reset and try again :)
 
     Congrats, you just created a bot that connects to the internet!
+
+    
+
+
 */
 #include <Arduino.h>
 
@@ -27,7 +39,7 @@
 #include <ESP8266HTTPClient.h>
 
 #define USE_SERIAL Serial
-#define SERVER_NAME "http://www.hmpg.net/" // put the host name here. this is a sample
+#define SERVER_NAME "http://www.hmpg.net/" // put the host name here like http://www.google.com . this is an example that works :)
 #define WIFI_SSID "YOUR-WiFi-NAME"
 #define WIFI_PASS "YOUR-WiFi-PASSWORD"
 
@@ -35,9 +47,9 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
 
-    USE_SERIAL.begin(9600);
-   // USE_SERIAL.setDebugOutput(true);
+    USE_SERIAL.begin(9600); // Init Serial comms
 
+    // Clear Serial buffer
     USE_SERIAL.println();
     USE_SERIAL.println();
     USE_SERIAL.println();
@@ -47,6 +59,7 @@ void setup() {
         delay(1000);
     }
 
+    // Add access point to WiFimulti
     WiFiMulti.addAP(WIFI_SSID, WIFI_PASS);
 
 }
@@ -55,23 +68,23 @@ void loop() {
     // wait for WiFi connection
     if((WiFiMulti.run() == WL_CONNECTED)) {
 
-        HTTPClient http;
 
+        // configure and initiate connection with target server and url
+        HTTPClient http;
         USE_SERIAL.print("[HTTP] starting...\n");
-        // configure traged server and url
-        http.begin(SERVER_NAME); //HTTP
+        http.begin(SERVER_NAME);
 
         USE_SERIAL.print("[HTTP] GET...\n");
         // start connection and send HTTP header
         int httpCode = http.GET();
 
-        // httpCode will be negative on error
+        // httpCode will be negative on error. Success http code is 200
         if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
             USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
-            // file found at server
+            // receive response from the Server
             if(httpCode == HTTP_CODE_OK) {
+                // On successful connection
                 USE_SERIAL.print("[HTTP] Received HTML...\n");
                 String payload = http.getString();
                 USE_SERIAL.println(payload);
@@ -83,7 +96,7 @@ void loop() {
 
         http.end();
     }
-
+    // 10 second delay, since we don't want to overload the target Server
     delay(10000);
 }
 
